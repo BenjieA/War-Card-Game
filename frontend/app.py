2f0d68ac83ba26a27c5beca5f63dcebb3cca5a93
Ubuntu <project@ori.cam4iz0pv23edh0knvvh25bpyd.zx.internal.cloudapp.net>
2020-04-04 19:19:45 +0000

    formatted

--- frontend/app.py
+++ frontend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, Response
+from flask import Flask, request, jsonify, Response, render_template
 import requests
 
 app = Flask(__name__)
@@ -9,13 +9,14 @@ def index():
 	resp_back = requests.get('http://backend:5001/')
 	results = str(resp_back.text)
 
-	draw = results.split(",")
-	draw_house = draw[0][15:-1]
-	draw_user = draw[1]
-	draw_win_card = draw[2][15:-1]
-	draw_win = draw[3][15:-4]
+	draw_house = None
+	draw_user = None
+	draw_win_card = None
+	draw_win = None
 
-	return draw_house
+	draw = results.split(",")
+
+	return render_template('index.html', draw_house=draw[0][15:-1], draw_user = draw[1][15:-1], draw_win_card = draw[2][15:-1], draw_win = draw[3][15:-4])	
 
 if  __name__ == '__main__':
 	app.run(debug=True, host='0.0.0.0', port=5000)
