7c31f1b3c7c4c94c1f4ecaa5d6b10b318a715f20
Ubuntu <project@ori.cam4iz0pv23edh0knvvh25bpyd.zx.internal.cloudapp.net>
2020-04-05 18:04:03 +0000

    finished up to nginx

--- frontend/app.py
+++ frontend/app.py
@@ -1,8 +1,12 @@
 from flask import Flask, request, jsonify, Response, render_template
 import requests
+from os import getenv
 
 app = Flask(__name__)
 from flask import Flask
+
+app.config['Version'] = getenv('Version')
+Version = getenv('Version')
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -15,8 +19,13 @@ def index():
 	draw_win = None
 
 	draw = results.split(",")
+	if Version == "v1":
+		return render_template('index.html', draw_house=draw[0][15:-1], draw_user = draw[1][15:-1], draw_win_card = draw[2][15:-1], draw_win = draw[3][15:-4])	
+	elif Version=="v2":
+		return render_template('index_v2.html', draw_house=draw[0][15:-1], draw_user = draw[1][15:-1], draw_win_card = draw[2][15:-1], draw_win = draw[3][15:-4])  
 
-	return render_template('index.html', draw_house=draw[0][15:-1], draw_user = draw[1][15:-1], draw_win_card = draw[2][15:-1], draw_win = draw[3][15:-4])	
+
+
 
 if  __name__ == '__main__':
 	app.run(debug=True, host='0.0.0.0', port=5000)
